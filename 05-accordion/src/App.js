fc74356bc18ff66efb56e358f28bfefff76798fd
cansualtun <altuncansu98@gmail.com>
2022-08-10 17:59:54 +0300

    files name changed

---
+++ 05-accordion/src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return <h2>accordion project setup</h2>;
+}
+
+export default App;
