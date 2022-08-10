8b9aab26903cfd36739ec02fc6b1e86fc5b64501
cansualtun <altuncansu98@gmail.com>
2022-08-10 18:37:03 +0300

    finishproject

--- 05-accordion/src/App.js
+++ 05-accordion/src/App.js
@@ -3,7 +3,20 @@ import data from "./data";
 import SingleQuestion from "./Question";
 function App() {
   const [questions, setQuestions] = useState(data);
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
