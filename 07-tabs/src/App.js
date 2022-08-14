e9f9898cb1dfe8964d544f17a2a9c181fa4db9a7
cansualtun <altuncansu98@gmail.com>
2022-08-14 23:38:09 +0300

    datafetch

--- 07-tabs/src/App.js
+++ 07-tabs/src/App.js
@@ -25,7 +25,30 @@ function App() {
       </section>
     );
   }
-  return <h2>tabs project setup</h2>;
+  const { company, dates, duties, title } = jobs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App;
